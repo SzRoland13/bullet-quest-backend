099adba4b7d4b044bc6cafe4cbea82cf5a7c3f97
Szabo Roland <szabo.roland.1213@gmail.com>
2025-05-27 18:50:03 +0200

    Initial commit

---
+++ src/main/java/dev/roland/bullet_quest_backend/BulletQuestBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.roland.bullet_quest_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletQuestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulletQuestBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/roland/bullet_quest_backend/BulletQuestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.roland.bullet_quest_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletQuestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
